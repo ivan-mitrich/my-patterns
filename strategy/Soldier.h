6a0f3983b96933a7c7e916f6b122f528ced5a8c7
Ivan <iskolyar@gmail.com>
2022-01-05 20:51:05 +0200

    added weapon user class

---
+++ strategy/Soldier.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "IWeapon.h"
+
+class Soldier
+{
+public:
+	Soldier() : weapon(nullptr) {}
+
+	void takeWeapon(IWeapon* weapon) {
+		dropWeapon();
+		this->weapon = weapon;
+	}
+
+	void dropWeapon() {
+		this->weapon = nullptr;
+	}
+
+	void fire() {
+		this->weapon->fire();
+	}
+
+	~Soldier() { dropWeapon(); }
+private:
+	IWeapon* weapon;
+};
+
